f0a43497b9e1114283aca6525813b6932443de07
ezeYaniv <brener.yaniv@gmail.com>
2021-05-03 23:26:35 -0700

    youtube and wikipedia keys added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
+import youtube from './api/youtube';
 import Form from './components/Form';
+import ResultList from './components/ResultList';
+import axios from 'axios';
 
 const App = () => {
 	// Variable declarations
@@ -15,9 +18,9 @@ const App = () => {
 			};
 		}, {})
 	);
+	const [results, setResults] = useState([]);
 
 	// Event handlers
-
 	const handleTermChange = (e) => setTerm(e.target.value);
 
 	const handleCheckboxChange = (e) => {
@@ -32,12 +35,48 @@ const App = () => {
 
 	const handleFormSubmit = (e) => {
 		e.preventDefault();
-        
+
+		// youtube api
+		if (searchEngines['YouTube'] && term) {
+			const youtubeSearch = async () => {
+				const response = await youtube.get('search', {
+					params: { q: term },
+				});
+
+				const youtubeResults = response.data.items.map((item) => item.snippet);
+				youtubeResults.forEach((item) => console.log(item.title));
+				// const [title, description] = youtubeResults;
+				// const resultDefault = {engine: 'YouTube', title: title}
+				console.log(youtubeResults);
+				// console.log(resultDefault);
+			};
+			youtubeSearch();
+		}
+
+		//wikipedia api
+		if (searchEngines['Wikipedia'] && term) {
+			const wikiSearch = async () => {
+				const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+					params: {
+						action: 'query',
+						format: 'json',
+						list: 'search',
+						origin: '*',
+						srsearch: term,
+					},
+				});
+
+				// if (data.query) {
+					console.log(data.query.search);
+				// }
+			};
+			wikiSearch();
+		}
 	};
 
 	// JSX rendering
 	return (
-		<React.Fragment>
+		<div>
 			<Form
 				term={term}
 				searchEngines={searchEngines}
@@ -45,7 +84,9 @@ const App = () => {
 				handleCheckboxChange={handleCheckboxChange}
 				handleFormSubmit={handleFormSubmit}
 			/>
-		</React.Fragment>
+			<hr></hr>
+			<ResultList results={results} />
+		</div>
 	);
 };
 
---
+++ src/components/ResultList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Result from './Result';
+
+const ResultList = () => {
+    return <Result />
+}
+
+export default ResultList;
